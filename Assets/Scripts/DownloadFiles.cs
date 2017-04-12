c1156b20a34140b6c457df213f5a1a8813d4f619
zhuwenhai <zhwh@mr214.cn>
2017-04-12 18:52:39 +0800

    update

--- Assets/Scripts/DownloadFiles.cs
+++ Assets/Scripts/DownloadFiles.cs
@@ -3,30 +3,51 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class DownloadFiles : MonoBehaviour {
+
+public class HotFile
+{
+    public double size;
+    public string url;
+}
+public class CalcProgress
+{
+    public WWW task;
+    public HotFile hotFile;
+}
+
+public class DownloadFiles : MonoBehaviour
+{
 
     List<Coroutine> listCoroutine = new List<Coroutine>();
     int completeTimes = 0;
     int tempcompleteTimes = 0;
-    int coroutineNums = 5;//最多多少协程用于下载文件
-    public void Download(List<string> updateFiles) {
-        if (updateFiles != null && updateFiles.Count > 0) {
+    int coroutineNums = 1;//最多多少协程用于下载文件
+    public void Download(List<HotFile> updateFiles)
+    {
+        if (updateFiles != null && updateFiles.Count > 0)
+        {
             int allCount = updateFiles.Count;//要下载的文件数量
-            if (allCount <= coroutineNums) {
+            if (allCount <= coroutineNums)
+            {
                 completeTimes = 1;
                 Coroutine coroutine = StartCoroutine("StartDownload", updateFiles);
                 listCoroutine.Add(coroutine);
-            } else {
+            }
+            else
+            {
                 completeTimes = coroutineNums;
                 int count = 0;
-                List<string> temp = null;
+                List<HotFile> temp = null;
                 Coroutine coroutine = null;
                 int spanNums = allCount / coroutineNums;
                 int tempNums = spanNums * coroutineNums;
                 int lastAddNums = allCount - tempNums;
-                for (int i = 0; i < coroutineNums; i++) {
+                print(coroutineNums + "个携程");
+                for (int i = 0; i < coroutineNums; i++)
+                {
                     count = spanNums;
-                    if (i == coroutineNums - 1) {
+                    if (i == coroutineNums - 1)
+                    {
                         count = spanNums + lastAddNums;
                     }
                     temp = updateFiles.GetRange(0 + i * spanNums, count);
@@ -37,12 +58,16 @@ public class DownloadFiles : MonoBehaviour {
         }
     }
 
-    void Update() {
-        if (completeTimes > 0 && tempcompleteTimes == completeTimes) {
+    void Update()
+    {
+        if (completeTimes > 0 && tempcompleteTimes == completeTimes)
+        {
             completeTimes = 0;
             tempcompleteTimes = 0;
-            if (listCoroutine != null && listCoroutine.Count > 0) {
-                for (int i = 0; i < listCoroutine.Count; i++) {
+            if (listCoroutine != null && listCoroutine.Count > 0)
+            {
+                for (int i = 0; i < listCoroutine.Count; i++)
+                {
                     StopCoroutine(listCoroutine[i]);
                     listCoroutine[i] = null;
                 }
@@ -50,28 +75,45 @@ public class DownloadFiles : MonoBehaviour {
             listCoroutine = new List<Coroutine>();
             print("全部下载完成!");
         }
+        for (int i = 0; i < calcProgressList.Count; i++)
+        {
+            CalcProgress calcProgress = calcProgressList[i];
+            currentAllSize[calcProgress.hotFile.url] = calcProgress.task.progress * calcProgress.hotFile.size;
+        }
     }
-
-    IEnumerator StartDownload(List<string> file) {
-        if (file != null && file.Count > 0) {
+    List<CalcProgress> calcProgressList = new List<global::CalcProgress>();
+    IEnumerator StartDownload(List<HotFile> file)
+    {
+        if (file != null && file.Count > 0)
+        {
             string singlefile = string.Empty;
             WWW Task = null;
-            for (int i = 0; i < file.Count; i++) {
-                singlefile = file[i];
-                if (!string.IsNullOrEmpty(singlefile)) {
+            for (int i = 0; i < file.Count; i++)
+            {
+                singlefile = file[i].url;
+                if (!string.IsNullOrEmpty(singlefile))
+                {
                     Task = new WWW(singlefile);
+                    CalcProgress dic = new CalcProgress();
+                    dic.task = Task;
+                    dic.hotFile = file[i];
+                    calcProgressList.Add(dic);
                     yield return Task;
-                    if (Task != null) {
-                        if (!string.IsNullOrEmpty(Task.error)) {
+                    if (Task != null)
+                    {
+                        if (!string.IsNullOrEmpty(Task.error))
+                        {
                             print("下载失败");
-                            Task.Dispose();
-                            Task = null;
-                        } else if (Task.isDone) {
+                            //Task.Dispose();
+                            //Task = null;
+                        }
+                        else if (Task.isDone)
+                        {
                             string[] names = singlefile.Split(new string[] { "/" }, System.StringSplitOptions.None);
                             string filename = names[names.Length - 1];
-                            SaveFileToLocal(Task, filename);
-                            Task.Dispose();
-                            Task = null;
+                            SaveFileToLocal(Task.bytes, "d:/" + filename);
+                            //Task.Dispose();
+                            //Task = null;
                             print("下载成功" + filename);
                         }
                     }
@@ -81,16 +123,31 @@ public class DownloadFiles : MonoBehaviour {
         tempcompleteTimes++;
         yield return null;
     }
+    public Dictionary<string, double> currentAllSize = new Dictionary<string, double>();
+    IEnumerator CalcProgress(CalcProgress calcProgress)
+    {
+        while (calcProgress.task.progress < 1)
+        {
+            currentAllSize[calcProgress.hotFile.url] = calcProgress.task.progress * calcProgress.hotFile.size;
+        }
+        currentAllSize[calcProgress.hotFile.url] = calcProgress.task.progress * calcProgress.hotFile.size;
+        yield return null;
+    }
 
-    public void SaveFileToLocal(WWW task, string fileName) {
-        if (task != null) {
-            byte[] bytes = task.bytes;
-            using (var ms = new MemoryStream(bytes)) {
-                string file = "d:/" + fileName;
-                using (Stream fs = new FileStream(file, FileMode.Create)) {
+
+
+    public void SaveFileToLocal(byte[] bytes, string filePath)
+    {
+        if (bytes != null && bytes.Length > 0)
+        {
+            using (var ms = new MemoryStream(bytes))
+            {
+                using (Stream fs = new FileStream(filePath, FileMode.Create))
+                {
                     byte[] b = new byte[1024 * 1024 * 4];
                     int getv = 0;
-                    while ((getv = ms.Read(b, 0, b.Length)) > 0) {
+                    while ((getv = ms.Read(b, 0, b.Length)) > 0)
+                    {
                         fs.Write(b, 0, getv);
                     }
                 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,33 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
 
-    List<string> updateFiles1 = new List<string>() {
-        "http://www.hotupdate.com/ab/1.zip",
-        "http://www.hotupdate.com/ab/2.zip",
-        "http://www.hotupdate.com/ab/3.zip",
-        "http://www.hotupdate.com/ab/4.zip",
-        "http://www.hotupdate.com/ab/5.zip",
-        "http://www.hotupdate.com/ab/6.zip",
-        "http://www.hotupdate.com/ab/7.zip",
+    List<HotFile> updateFiles1 = new List<HotFile>() {
+        new HotFile() { size = 103322224, url = "http://www.hotupdate.com/ab/1.zip" },
+        new HotFile() { size = 19071619, url = "http://www.hotupdate.com/ab/2.zip"},
+        new HotFile() { size = 20777879, url = "http://www.hotupdate.com/ab/3.zip" },
+        new HotFile() { size = 23822, url = "http://www.hotupdate.com/ab/4.zip"},
+        new HotFile() { size = 84323, url = "http://www.hotupdate.com/ab/5.zip"},
+        new HotFile() { size = 110628, url = "http://www.hotupdate.com/ab/6.zip"},
+        new HotFile() { size = 1486058, url = "http://www.hotupdate.com/ab/7.zip"},
+        new HotFile() { size = 294906, url =  "http://www.hotupdate.com/ab/8.zip"},
+        new HotFile() { size = 126255, url =  "http://www.hotupdate.com/ab/9.zip"},
+        new HotFile() { size = 49140, url = "http://www.hotupdate.com/ab/10.zip"},
+        new HotFile() { size = 8381791, url = "http://www.hotupdate.com/ab/11.zip"},
+        new HotFile() { size = 44287, url = "http://www.hotupdate.com/ab/12.zip"},
+        new HotFile() { size = 339696, url = "http://www.hotupdate.com/ab/13.zip"},
     };
-    List<string> updateFiles2 = new List<string>() {
-        "http://www.hotupdate.com/ab/8.zip",
-        "http://www.hotupdate.com/ab/9.zip",
-        "http://www.hotupdate.com/ab/10.zip",
-        "http://www.hotupdate.com/ab/11.zip",
-        "http://www.hotupdate.com/ab/12.zip",
-        "http://www.hotupdate.com/ab/13.zip",
-    };
-    void Start() {
-        GetComponent<DownloadFiles>().Download(updateFiles1);
+    double allSize = 154112628;
+    DownloadFiles downloadFiles;
+    void Start()
+    {
+        downloadFiles = GetComponent<DownloadFiles>();
+        downloadFiles.Download(updateFiles1);
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.A)) {
-            GetComponent<DownloadFiles>().Download(updateFiles2);
+    double currentSize = 0;
+    void OnGUI()
+    {
+        currentSize = 0;
+        foreach (var item in downloadFiles.currentAllSize)
+        {
+            currentSize += item.Value;
         }
+        GUILayout.Label((currentSize + " = " + allSize).ToString());
+        GUILayout.Label((currentSize / allSize).ToString("0.##"));
     }
 
 
